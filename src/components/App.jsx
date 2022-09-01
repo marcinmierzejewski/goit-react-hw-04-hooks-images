55fe865f1a47e487d42465b5096fc79c779657a1
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-09-01 21:55:25 +0200

    add hooks to App with warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+
 import { SearchBar } from './SearchBar/SearchBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
@@ -6,121 +7,244 @@ import { Button } from './Button/Button';
 import { Modal } from './Modal/Modal';
 import { fetchPhotos } from 'api/fetchPhotos';
 
-export class App extends Component {
-  state = {
-    pictures: [],
-    page: 1,
-    search: '',
-    nextSearch: '',
-    isLoading: false,
-    isModalOpen: false,
-    modalImg: null,
-    error: null,
-  };
+export const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [nextSearch, setNextSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImg, setModalImg] = useState(null);
+  const [error, setError] = useState(null);
 
-  updatePictures = async newSearch => {
-    const { page, pictures, search } = this.state;
+  const updatePictures = async newSearch => {
 
     try {
+      setIsLoading(() => true)
       const photos = await fetchPhotos(newSearch, page);
       const oldPictures = pictures;
       if (photos.length !== 0) {
         const newPictures = [...oldPictures, ...photos];
         if (search !== newSearch) {
-          this.setState({ pictures: photos, page: 1 });
+          setPictures(() => photos);
+          setPage(() => 1);
         }
         if (search === newSearch) {
-          this.setState({ pictures: newPictures, page: page + 1 });
+          setPictures(() => newPictures);
+          setPage((oldPage) => oldPage + 1);
         }
       } else {
         alert('Sorry, no image matching');
       }
     } catch (error) {
-      this.setState({ error });
+      setError(() => error)
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(() => false)
     }
   };
 
-  resetArray = searchPicture => {
-    this.setState({
-      search: searchPicture,
-      isLoading: true,
-      pictures: [],
-      page: 1,
-    });
+  const resetArray = searchPicture => {
+    setSearch(() => searchPicture);
+    setPictures(() => []);
+    setPage(() => 1);
   };
 
-  changeSearchValue = ({ searchPicture }) => {
-    this.resetArray(searchPicture);
-    this.updatePictures(searchPicture);
+  const changeSearchValue = ({ searchPicture }) => {
+    resetArray(searchPicture);
+    updatePictures(searchPicture);
   };
 
-  loadMorePictures = () => {
-    const { nextSearch } = this.state;
-    this.updatePictures(nextSearch);
+  const loadMorePictures = () => {
+    updatePictures(nextSearch);
   };
 
-  async componentDidUpdate() {
-    const { search, nextSearch } = this.state;
+  useEffect (() => {
     if (nextSearch !== search) {
-      this.updatePictures(search);
-      this.setState({ nextSearch: search });
+      updatePictures(search);
+      setNextSearch(search);
     }
-  }
+  }, [search, nextSearch]);
 
-  openModalWindow = e => {
+  const openModalWindow = e => {
     const largeImg = e.target.dataset.source;
     if (e.target.nodeName !== 'IMG') {
       return;
     }
-    this.setState({
-      modalImg: largeImg,
-      isModalOpen: true,
-    });
+    setModalImg(() => largeImg);
+    setIsModalOpen(() => true); 
   };
 
-  closeModalWindow = e => {
+  const closeModalWindow = e => {
     if (e.target.nodeName === 'IMG') {
       return;
     }
-    this.setState({ isModalOpen: false });
+    setIsModalOpen(() => false);
   };
 
-  render() {
-    const { error, pictures, isLoading, isModalOpen, modalImg } = this.state;
-
+  useEffect(() => {
     if (isModalOpen) {
       window.addEventListener('keydown', e => {
         if (e.code === 'Escape') {
-          this.setState({ isModalOpen: false });
+          setIsModalOpen(() => false);
         }
       });
     }
+  }, [isModalOpen])
+
+    
 
     return (
       <div>
-        <SearchBar newSearch={this.changeSearchValue} />
+        <SearchBar newSearch={changeSearchValue} />
         {error && <p>ERROR: Whoops O_o, something went wrong: {error.message}</p>}
         {isLoading ? (
           <Loader />
         ) : (
           <ImageGallery
             pictures={pictures}
-            openModalWindow={this.openModalWindow}
+            openModalWindow={openModalWindow}
           />
         )}
         {pictures.length !== 0 ? (
-          <Button text="Load more" func={this.loadMorePictures} />
+          <Button text="Load more" func={loadMorePictures} />
         ) : (
           ''
         )}
         {isModalOpen ? (
-          <Modal modalImgLarge={modalImg} closeImg={this.closeModalWindow} />
+          <Modal modalImgLarge={modalImg} closeImg={closeModalWindow} />
         ) : (
           <></>
         )}
       </div>
     );
   }
-}
+
+
+
+// import { useState, useEffect } from 'react';
+
+// import { SearchBar } from './SearchBar/SearchBar';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Loader } from './Loader/Loader';
+// import { Button } from './Button/Button';
+// import { Modal } from './Modal/Modal';
+// import { fetchPhotos } from 'api/fetchPhotos';
+
+// export class App extends Component {
+//   state = {
+//     pictures: [],
+//     page: 1,
+//     search: '',
+//     nextSearch: '',
+//     isLoading: false,
+//     isModalOpen: false,
+//     modalImg: null,
+//     error: null,
+//   };
+
+//   updatePictures = async newSearch => {
+//     const { page, pictures, search } = this.state;
+
+//     try {
+//       const photos = await fetchPhotos(newSearch, page);
+//       const oldPictures = pictures;
+//       if (photos.length !== 0) {
+//         const newPictures = [...oldPictures, ...photos];
+//         if (search !== newSearch) {
+//           this.setState({ pictures: photos, page: 1 });
+//         }
+//         if (search === newSearch) {
+//           this.setState({ pictures: newPictures, page: page + 1 });
+//         }
+//       } else {
+//         alert('Sorry, no image matching');
+//       }
+//     } catch (error) {
+//       this.setState({ error });
+//     } finally {
+//       this.setState({ isLoading: false });
+//     }
+//   };
+
+//   resetArray = searchPicture => {
+//     this.setState({
+//       search: searchPicture,
+//       isLoading: true,
+//       pictures: [],
+//       page: 1,
+//     });
+//   };
+
+//   changeSearchValue = ({ searchPicture }) => {
+//     this.resetArray(searchPicture);
+//     this.updatePictures(searchPicture);
+//   };
+
+//   loadMorePictures = () => {
+//     const { nextSearch } = this.state;
+//     this.updatePictures(nextSearch);
+//   };
+
+//   async componentDidUpdate() {
+//     const { search, nextSearch } = this.state;
+//     if (nextSearch !== search) {
+//       this.updatePictures(search);
+//       this.setState({ nextSearch: search });
+//     }
+//   }
+
+//   openModalWindow = e => {
+//     const largeImg = e.target.dataset.source;
+//     if (e.target.nodeName !== 'IMG') {
+//       return;
+//     }
+//     this.setState({
+//       modalImg: largeImg,
+//       isModalOpen: true,
+//     });
+//   };
+
+//   closeModalWindow = e => {
+//     if (e.target.nodeName === 'IMG') {
+//       return;
+//     }
+//     this.setState({ isModalOpen: false });
+//   };
+
+//   render() {
+//     const { error, pictures, isLoading, isModalOpen, modalImg } = this.state;
+
+//     if (isModalOpen) {
+//       window.addEventListener('keydown', e => {
+//         if (e.code === 'Escape') {
+//           this.setState({ isModalOpen: false });
+//         }
+//       });
+//     }
+
+//     return (
+//       <div>
+//         <SearchBar newSearch={this.changeSearchValue} />
+//         {error && <p>ERROR: Whoops O_o, something went wrong: {error.message}</p>}
+//         {isLoading ? (
+//           <Loader />
+//         ) : (
+//           <ImageGallery
+//             pictures={pictures}
+//             openModalWindow={this.openModalWindow}
+//           />
+//         )}
+//         {pictures.length !== 0 ? (
+//           <Button text="Load more" func={this.loadMorePictures} />
+//         ) : (
+//           ''
+//         )}
+//         {isModalOpen ? (
+//           <Modal modalImgLarge={modalImg} closeImg={this.closeModalWindow} />
+//         ) : (
+//           <></>
+//         )}
+//       </div>
+//     );
+//   }
+// }
